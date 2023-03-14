b0003886123452a04b10272d5fd32e80db6cd44a
davidbbisht <davidbisht.bisht@gmail.com>
2023-03-14 13:50:27 +0530

    homepagee

--- src/components/constants/images.js
+++
@@ -1,5 +0,0 @@
-import nikeLogo from '../../assets/Nike-logo.png';
-import nikeLogoPayment from '../../assets/nikeLogoPayment.png'
-
-
-export { nikeLogo, nikeLogoPayment };
--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider as ReduxProvider } from "react-redux";
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
-import axios from "axios";
-import { theme } from "./theme";
-import { store } from "./redux/store/store";
+import { ColorModeScript } from '@chakra-ui/react';
+import React, { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
-axios.defaults.baseURL = "https://backend-nike.onrender.com";
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ReduxProvider store={store}>
-      <BrowserRouter>
-        <ChakraProvider theme={theme}>
-          <App />
-        </ChakraProvider>
-      </BrowserRouter>
-    </ReduxProvider>
-  </React.StrictMode>
+  <StrictMode>
+    <ColorModeScript />
+    <App />
+  </StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
